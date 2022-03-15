7d0c0a246d4929c2fee51a1ba17bd05456c44e48
RafaelBira <rafaelgiuliani443@gmail.com>
2022-03-15 15:08:03 -0300

    adicionado projeto aula01

---
+++ aula01/src/Exercicios01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicios01 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("digite a primeira nota");
+		double nota1 = teclado.nextDouble();
+
+		System.out.println("digite a segunda nota");
+		double nota2 = teclado.nextDouble();
+		double media = (nota1 + nota2) / 2;
+		System.out.println("a nota média é " + media);
+	}
+
+}
