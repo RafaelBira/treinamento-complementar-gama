abaff8622719e7647e53df6843e8fc52d7e5e405
RafaelBira <rafaelgiuliani443@gmail.com>
2022-03-15 15:36:58 -0300

    adicionado exercicio02

---
+++ aula01/src/Exercicio02.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		double salarioBruto, prestacao, valorLimite;
+		System.out.println("digite o valor do salário bruto");
+		salarioBruto = teclado.nextDouble();
+		System.out.println("digite o valor da prestação");
+		prestacao = teclado.nextDouble();
+		valorLimite = salarioBruto * 0.3;
+		if (prestacao >= valorLimite) {
+			System.out.println("crédito negado");
+		} else {
+			System.out.println("crédito aprovado");
+		}
+
+	}
+}
