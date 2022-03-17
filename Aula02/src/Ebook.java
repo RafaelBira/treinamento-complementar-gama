d35a2248d5f1c4bd89d2ba8e7778a7e602c7ef6e
RafaelBira <rafaelgiuliani443@gmail.com>
2022-03-17 16:16:28 -0300

    adicionado exercicio ebook

---
+++ Aula02/src/Ebook.java
@@ -0,0 +1,48 @@
+
+public class Ebook {
+	private String titulo;
+	private String autor;
+	private int totalPaginas;
+	private int paginaAtual = 1;
+
+	public Ebook(String titulo, String autor, int totalPaginas) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.totalPaginas = totalPaginas;
+	}
+
+	public int getPaginaAtual() {
+		return paginaAtual;
+	}
+
+	public void setPaginaAtual(int paginaAtual) {
+		this.paginaAtual = paginaAtual;
+	}
+
+	public void avançarPagina(int quantas) {
+		if (paginaAtual < totalPaginas) {
+			paginaAtual += quantas;
+		}
+	}
+
+	public void voltarPagina(int quantas) {
+		if (paginaAtual > 1) {
+			paginaAtual -= quantas;
+		}
+	}
+
+	public void irParaPagina(int paginaInformada) {
+		if (paginaInformada > 0 && paginaInformada < totalPaginas) {
+			paginaAtual = paginaInformada;
+		}
+	}
+
+	public void mostrarPaginaAtual() {
+		System.out.println("você está na página " + paginaAtual);
+	}
+
+	public void mostrarInfos() {
+		System.out.println("título: " + titulo + "\r\nautor: " + autor + "\r\ntotal de páginas: " + totalPaginas);
+	}
+
+}
---
+++ Aula02/src/TestarEbook.java
@@ -0,0 +1,15 @@
+
+public class TestarEbook {
+
+	public static void main(String[] args) {
+Ebook e=new Ebook("a volta dos que não foram", "Rafael Mito Giuliani", 33);
+e.mostrarInfos();
+e.avançarPagina(1);
+e.mostrarPaginaAtual();
+e.voltarPagina(1);
+e.mostrarPaginaAtual();
+e.irParaPagina(25);
+e.mostrarPaginaAtual();
+	}
+
+}
